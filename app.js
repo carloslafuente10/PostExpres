913d520ff7fdb90c96101159fe6820797cf6b38a
La Fuente Canaviri Carlos Aurelio <carlos.aurelio.la.ca@gmail.com>
2025-09-16 18:09:52 -0400

    Modificación de puerto dinamico

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 //const port = 3000;
@@ -12,6 +13,6 @@ app.get('/', (req, res) => {
     `);
 });
 app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`);
+    console.log(`Servidor: http://localhost:${PORT}`);
 });
 
